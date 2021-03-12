bf605899beb322fd2a8c22a9419e1b790ccbee4a
Riccardo Geri <geririccardo34@gmail.com>
2021-03-13 00:16:22 +0100

    Add queryselector for all buttons

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-let x = "0";
+const clearButton = document.querySelector('#clear-btn');
+const divisionButton = document.querySelector('#division-btn');
+const sevenButton = document.querySelector('#seven-btn');
+const eightButton = document.querySelector('#eight-btn');
+const nineButton = document.querySelector('#nine-btn');
+const multiplicationButton = document.querySelector('#multiplication-btn');
+const fourButton = document.querySelector('#four-btn');
+const fiveButton = document.querySelector('#five-btn');
+const sixButton = document.querySelector('#six-btn');
+const subtractionButton = document.querySelector('#subtraction-btn');
+const oneButton = document.querySelector('#one-btn');
+const twoButton = document.querySelector('#two-btn');
+const threeButton = document.querySelector('#three-btn');
+const additionButton = document.querySelector('#addition-btn');
+const zeroButton = document.querySelector('#zero-btn');
+const dotButton = document.querySelector('#dot-btn');
+const equalsButton = document.querySelector('#equals-btn');
 
 add = (a, b) => a + b;
 subtract = (a, b) => a - b;
