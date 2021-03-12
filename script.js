340e8d168585a667cc3009a7b4ac3c32ed63024f
Riccardo Geri <geririccardo34@gmail.com>
2021-03-13 00:58:15 +0100

    Make it possible to write numbers

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
-const clearButton = document.getElementById('#clear-btn');
-const divisionButton = document.getElementById('#division-btn');
-const sevenButton = document.getElementById('#seven-btn');
-const eightButton = document.getElementById('#eight-btn');
-const nineButton = document.getElementById('#nine-btn');
-const multiplicationButton = document.getElementById('#multiplication-btn');
-const fourButton = document.getElementById('#four-btn');
-const fiveButton = document.getElementById('#five-btn');
-const sixButton = document.getElementById('#six-btn');
-const subtractionButton = document.getElementById('#subtraction-btn');
-const oneButton = document.getElementById('#one-btn');
-const twoButton = document.getElementById('#two-btn');
-const threeButton = document.getElementById('#three-btn');
-const additionButton = document.getElementById('#addition-btn');
-const zeroButton = document.getElementById('#zero-btn');
-const dotButton = document.getElementById('#dot-btn');
-const equalsButton = document.getElementById('#equals-btn');
+const clearButton = document.getElementById('clear-btn');
+const divisionButton = document.getElementById('division-btn');
+const sevenButton = document.getElementById('seven-btn');
+const eightButton = document.getElementById('eight-btn');
+const nineButton = document.getElementById('nine-btn');
+const multiplicationButton = document.getElementById('multiplication-btn');
+const fourButton = document.getElementById('four-btn');
+const fiveButton = document.getElementById('five-btn');
+const sixButton = document.getElementById('six-btn');
+const subtractionButton = document.getElementById('subtraction-btn');
+const oneButton = document.getElementById('one-btn');
+const twoButton = document.getElementById('two-btn');
+const threeButton = document.getElementById('three-btn');
+const additionButton = document.getElementById('addition-btn');
+const zeroButton = document.getElementById('zero-btn');
+const dotButton = document.getElementById('dot-btn');
+const equalsButton = document.getElementById('equals-btn');
+let displayScreen = document.getElementById('display');
 
-let display = document.getElementById('#display')
+const numberButtons = [zeroButton, oneButton, twoButton, threeButton, fourButton, fiveButton, sixButton, sevenButton, eightButton, nineButton, dotButton];
+
+numberButtons.forEach((button) => button.addEventListener("click", () => writeNumber(button.textContent)));
+
+function writeNumber(number){
+    if(displayScreen.textContent === "0") wipeScreen();
+    if(displayScreen.textContent.length < 13) displayScreen.textContent += number;
+}
+
+wipeScreen = () => displayScreen.textContent = "";
 
 add = (a, b) => a + b;
 subtract = (a, b) => a - b;
